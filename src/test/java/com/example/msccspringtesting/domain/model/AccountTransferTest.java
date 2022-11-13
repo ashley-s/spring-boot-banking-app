65086ff8bc0362b95ef7dbef3038659a57d6ef7e
Ashley Shookhye <ashley.shookhye@gmail.com>
2022-11-13 12:41:09 +0400

    initial commit

--- src/test/java/com/example/msccspringtesting/domain/model/AccountTransferTest.java
+++ src/test/java/com/example/msccspringtesting/domain/model/AccountTransferTest.java
@@ -3,8 +3,6 @@ package com.example.msccspringtesting.domain.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class AccountTransferTest {
 
     @Test
